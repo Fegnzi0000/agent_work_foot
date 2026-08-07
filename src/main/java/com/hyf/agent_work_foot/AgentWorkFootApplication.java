f0ad6de2fe2ee977c4e16974971ce968d3a4284a
Huang_yaofeng <2680270733@qq.com>
2026-08-07 22:52:29 +0800

    创建项目

---
+++ src/main/java/com/hyf/agent_work_foot/AgentWorkFootApplication.java
@@ -0,0 +1,13 @@
+package com.hyf.agent_work_foot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentWorkFootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentWorkFootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyf/agent_work_foot/AgentWorkFootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyf.agent_work_foot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentWorkFootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
